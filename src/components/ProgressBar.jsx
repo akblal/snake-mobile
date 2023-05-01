054b292f61d2807e321549fe319507b8dcd1228c
Brandon Hsu <brandon.j.hsu@gmail.com>
2023-05-01 16:04:47 -0700

    created random progress loading on select game modal

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -1,53 +1,96 @@
-import React, { useState } from 'react';
-import { View, TouchableOpacity, Text, StyleSheet, Modal, Pressable, SafeAreaView, Image } from 'react-native';
+import React, { useState, useEffect, useRef } from 'react';
+import { View, Text, StyleSheet, Animated} from 'react-native';
+
+
+function useInterval(callback, delay) {
+  const savedCallback = useRef();
+
+  // Remember the latest callback.
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  // Set up the interval.
+  useEffect(() => {
+    function tick() {
+      savedCallback.current();
+    }
+    if (delay !== null) {
+      let id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
+}
 
 const ProgressBar = () => {
 
-  const [width, setWidth] = useState(60);
+  let animation = useRef(new Animated.Value(0));
+  const [progress, setProgress] = useState(0);
+  useInterval(() => {
+    // update progress until 100
+    let update = Math.floor(Math.random() * 50);
+    if (progress + update > 100) {
+      update = 100 - progress;
+    }
+    if(progress < 100) {
+      setProgress(progress + update);
+    }
+  }, 1000);
 
+  useEffect(() => {
+    Animated.timing(animation.current, {
+      toValue: progress,
+      duration: 5,
+      useNativeDriver: false
+    }).start();
+  },[progress])
 
-  // const loading = () => {
-  //   let num = width;
-  //   num += 10;
-  //   setWidth(num);
-  // }
-
-  // while (width < 100) {
-  //   setTimeout(loading(), 100)
-  // }
-
+  const width = animation.current.interpolate({
+    inputRange: [0, 100],
+    outputRange: ["0%", "100%"],
+    extrapolate: "clamp"
+  })
 
   return (
+    <View style={styles.container}>
+      { progress === 100 ?
+        <Text>Loading Complete!</Text>  :
+        <Text>Loading...</Text>
+      }
 
-    <View >
-      <Text> Progress Bar </Text>
-      <View style= {[styles.done]}>
-        <View style= {[styles.hi, {width: width}]}>
-        </View>
+      <View style={styles.progressBar}>
+        <Animated.View style={[StyleSheet.absoluteFill, { backgroundColor: "teal", width }]}/>
       </View>
-    </View>
+      <Text>
+        {`${progress}%`}
+      </Text>
 
+    </View>
   )
+
+
 }
 
 const styles = StyleSheet.create({
-
-  done: {
-    display: 'flex',
-    height: 10,
-    transition: '1s, ease 0.3sec',
-    backgroundColor: 'green',
-    borderColor: 'black',
-    borderWidth: 1,
-    width: 100,
+  container: {
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'pink',
+    padding: 8,
+    width: '100%',
+    // borderColor: 'black',
+    // borderWidth: 1,
   },
-  hi: {
-    display: 'flex',
-    borderColor: 'yellow',
+  progressBar: {
+    height: 20,
+    flexDirection: "row",
+    backgroundColor: 'white',
+    borderColor: '#000',
     borderWidth: 1,
-    width: 30,
-    height: 10
-  }
+    width: '100%',
+  },
 });
 
 export default ProgressBar
